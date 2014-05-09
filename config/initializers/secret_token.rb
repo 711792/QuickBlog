cc56ec7babe82080eb29a54eacdf5d1ea66c019f
Haji Moosha <haji.moosha@tcs.com>
2014-05-09 16:05:49 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuickBlog::Application.config.secret_token = 'fb0ce869d539493167181bf1fbeaaef6f9786386019425cb19e1c44cbd55bfa113fa51e08d98c0d1643839cbf9090ee11bc2046ddf6c52f9ddbb1ab6029d8d60'
